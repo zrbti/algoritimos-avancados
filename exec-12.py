6b754e166d1630932650766a9772a69fd9b7a558
miguel <rouxinoldia@gmail.com>
2024-09-02 20:35:32 -0300

    Update exec-12.py

--- exec-12.py
+++ exec-12.py
@@ -1,1 +1,12 @@
+# 12) Escreva um algoritmo para ler uma temperatura em graus Fahrenheit, calcular e escrever o valor correspondente em graus Celsius (baseado na fórmula abaixo): 
+# C/5 =F - 32 /9
+# Observação: Para testar se a sua resposta está correta saiba que 100℃ = 212F
+
+tempofar=float(input("digite a temperatura em Fahrenheit"))
+c=((tempofar-32)/9)*5
+
+print(c)
+
+
+
 
