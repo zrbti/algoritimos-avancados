b96df77ef0e6ef2db08ee4941ecc580fe555b669
miguel <rouxinoldia@gmail.com>
2024-09-02 21:12:05 -0300

    Create exec-14.py

---
+++ exec-14.py
@@ -0,0 +1,9 @@
+#14) Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10!
+
+valor=float(input("digite um valor"))
+     
+if valor > 10:
+   print("é maior que 10")
+else:    
+   print("é menor que 10") 
+        
