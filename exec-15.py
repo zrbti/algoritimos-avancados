c973c8c2fc7dcfda0ad8bd4bf16f1b1cc4a09525
miguel <rouxinoldia@gmail.com>
2024-09-02 21:19:54 -0300

    Create exec-15.py

---
+++ exec-15.py
@@ -0,0 +1,10 @@
+#15) Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo). 
+
+valor=float(input("Insira um valor"))
+
+if valor >= 0:
+    print("é positivo")
+    
+else:
+    
+    print("é negativo")
